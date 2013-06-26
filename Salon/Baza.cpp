1b826a5f190e53e852c90b093f12d03082c3c024
Piotr Guzia <guziaster@gmail.com>
2013-06-26 22:53:44 +0200

    dodano mechanizm wynagrodzen do reszty klas

--- Salon/Baza.cpp
+++ Salon/Baza.cpp
@@ -1,4 +1,4 @@
 #include "Biblioteki.h"
 #include "Salon.h"
 
-Salon nowy("Jan","Nowak","2424244",10,"Opel",250000.00,50000.00,30000.00);
+Salon nowy("Jan","Nowak","2424244",10,"Opel",250000.00,50000.00,30000.00 , 10000); //usun¹³em wynagrodzenie dla prezesa, jest ono liczone do konstruktora z przychodów salonu i modyfikowane w zale¿noœci przepracowanych godzin dla prezesa
--- Salon/Manager.h
+++ Salon/Manager.h
@@ -2,17 +2,18 @@
 #include "Biblioteki.h"
 #include "Osoba.h"
 #include "UjemnaWartosc.h"
-
+#include "Samochod.h"
 class Manager : public Osoba
 {
-	private:
+private:
 	unsigned short int IloscPodwladnych;
 	double Wynagrodzenie;
 	unsigned short int CzasPracy;
+	std::vector<Samochod> SprzedaneSamochody;
 public:
 	Manager(const std::string imie, const std::string nazwisko, const std::string telefon, const double wynagrodzenie, const unsigned short int czas, const unsigned int podwladni);
-	//w tej klasie trzeba jeszcze cos wymyslec... narazie zostawiam same klasy w konstruktorami, metody wymyslimy pozniej
 	double NalezneWynagrodzenie();
+	void SprzedajSamochod(Samochod &S);
 	friend std::ostream & operator <<(std::ostream &Strumien, const Manager &M);
 	const char * getType(){	return typeid(this).name();}
 };
